94cad6573b66481b7dfdcd739bba814e083ac516
TheCrafter <dimitrisvlh@gmail.com>
2018-04-21 23:23:47 +0300

    Created tilemap instance

---
+++ assets/tilesets/tileset_DC-01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_DC-01" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="tileset_DC-01.png" width="96" height="64"/>
+</tileset>
---
+++ scenes/Background.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+    for c in $PATH.get_children():
+        var s = Sprite.new()
+        s.texture = load("res://assets/icon.png")
+        s.position.x = c.position.x
+        s.position.y = c.position.y
+        add_child(s)
