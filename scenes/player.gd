f6e254f848140974342ce6e355b3259a0920d420
TheArtist <agorglouk@gmail.com>
2018-04-21 21:39:39 +0300

    Added character scene

---
+++ scenes/player.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+export (int) var SPEED
+var checkpoints = []
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			checkpoints.push_back(event.position)
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	if checkpoints.size() > 0:
+		var target_pt = checkpoints[0]
+		var eps = 5
+		var dir = target_pt - position
+		if dir.length() > eps:
+			var velocity = dir.normalized() * SPEED
+			position += velocity * delta
+			$AnimatedSprite.animation = "right"
+			$AnimatedSprite.flip_h = velocity.x < 0
+			$AnimatedSprite.play()
+		else:
+			checkpoints.pop_front()
+			$AnimatedSprite.animation = "idle"
+			$AnimatedSprite.stop()
