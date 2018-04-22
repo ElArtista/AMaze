d1106102c35cfc77c58107cb064b2fbce30233ac
TheCrafter <dimitrisvlh@gmail.com>
2018-04-22 07:14:44 +0300

    Draw food items on board

--- scenes/Map/Map.gd
+++ scenes/Map/Map.gd
@@ -1,9 +1,31 @@
 extends Node2D
 
+var egg = load("res://assets/icons/food/egg.png")
+var apple = load("res://assets/icons/food/apple.png")
+var candy = load("res://assets/icons/food/candy.png")
+var grape = load("res://assets/icons/food/grape.png")
+var lemon = load("res://assets/icons/food/lemon.png")
+var carrot = load("res://assets/icons/food/carrot.png")
+var eggplant = load("res://assets/icons/food/eggplant.png")
+var icons = [egg, apple, candy, grape, lemon, carrot, eggplant]
+
+var path_points = Array()
+var item_points = Array()
+
+func new_sprite(texture, pos):
+    var s = Sprite.new()
+    s.texture = texture
+    s.position = pos
+    return s
+
 func _ready():
-	for c in $PATH.get_children():
-		var s = Sprite.new()
-		s.texture = load("res://assets/icon.png")
-		s.position.x = c.position.x
-		s.position.y = c.position.y
-		add_child(s)
+    for i in $PATH.get_children():
+        path_points.append(i.position)
+        add_child(new_sprite(load("res://assets/icon.png"), i.position))
+        
+    for i in $DIAMONDS.get_children():
+        item_points.append(i.position)
+        var r = randi() % icons.size()
+        var s = new_sprite(icons[r], i.position)
+        s.scale = Vector2(0.13, 0.13)
+        add_child(s)
