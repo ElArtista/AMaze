81d5d48888d2f6f9ddc8322ab0dd815c93f231e4
TheCrafter <dimitrisvlh@gmail.com>
2018-05-07 01:51:38 +0300

    Generate random win path on map

--- objects/gui/Level1.gd
+++ objects/gui/Level1.gd
@@ -2,6 +2,37 @@ extends Node2D
 
 const MAP_PROCESSOR = preload("res://objects/map/MapProcessor.gd")
 const APPLE = preload("res://assets/icons/food/apple.png")
+const EGG = preload("res://assets/icons/food/egg.png")
+
+const WIN_POINTS = 3
+
+func find_point_by_position(points, pos):
+	for p in points:
+		if p.position == pos:
+			return p
+	return null
+
+func random_win_path(points, start_point = points[randi() % points.size()], visited = Array()):
+	visited.append(start_point)
+	if visited.size() == WIN_POINTS:
+		return visited
+
+	# Find nearby points
+	var adjacent_points = Array()
+	for p in start_point.get_points():
+		adjacent_points.append(find_point_by_position(points, p.position))
+
+	# Remove visited points
+	for p in visited:
+		adjacent_points.remove(adjacent_points.find(p))
+
+	# Ensure we have movement left
+	if adjacent_points.size() > 0:
+		return random_win_path(points, adjacent_points[randi() % adjacent_points.size()], visited)
+	# If not restart algorithm
+	else:
+		visited.clear()
+		return random_win_path(points)
 
 func draw_line(p1, p2, color = Color(0,0,1)):
 	var line = Line2D.new()
@@ -23,11 +54,21 @@ func _ready():
 		s.position = point.position
 		add_child(s)
 
-		if point.left:
-			draw_line(point, point.left)
-		if point.right:
-			draw_line(point, point.right)
-		if point.up:
-			draw_line(point, point.up)
-		if point.down:
-			draw_line(point, point.down)
+#		if point.left:
+#			draw_line(point, point.left)
+#		if point.right:
+#			draw_line(point, point.right)
+#		if point.up:
+#			draw_line(point, point.up)
+#		if point.down:
+#			draw_line(point, point.down)
+
+	randomize()
+	var win_path = random_win_path(points)
+	for p in win_path:
+		var s = Sprite.new()
+		s.texture = EGG
+		s.scale = Vector2(0.2, 0.2)
+		s.position = p.position
+		add_child(s)
+	print(win_path)
--- objects/map/MapPoint.gd
+++ objects/map/MapPoint.gd
@@ -11,3 +11,15 @@ func _init(node):
 	#self = node.duplicate()
 	position = node.position
 	pass
+
+func get_points():
+	var p = Array()
+	if left != null:
+		p.append(left)
+	if right != null:
+		p.append(right)
+	if up != null:
+		p.append(up)
+	if down != null:
+		p.append(down)
+	return p
